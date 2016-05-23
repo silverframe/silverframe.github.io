048faa9dfdb071b42ab961d6750be5d37e65472d
silverframe <ibercombie@gmail.com>
2016-05-24 01:10:19 +0800

    mobile responsive details added

--- style/js/style.js
+++ style/js/style.js
@@ -7,22 +7,24 @@
        });
    });
 
-   var controller = new ScrollMagic.Controller();
+     var controller = new ScrollMagic.Controller();
 
-   var scene1 = new ScrollMagic.Scene({
-     triggerElement: "#about-me", // point of execution
-     duration: ($(window).height() * 0.5) , // pin element for the window height - 1
-     triggerHook: 0, // don't trigger until #about-me hits the top of the viewport
-     reverse: true // allows the effect to trigger when scrolled in the reverse direction
-   })
-   .setPin("#color-aboutme") // the element we want to pin
-   .addTo(controller);
 
-   var scene2 = new ScrollMagic.Scene({
-     triggerElement: "#portfolio", // point of execution
-     duration: ($(window).height() *3), // pin element for the window height - 1
-     triggerHook: 0, // don't trigger until #about-me hits the top of the viewport
-     reverse: true // allows the effect to trigger when scrolled in the reverse direction
-   })
-   .setPin("#color-portfolio") // the element we want to pin
-   .addTo(controller);
+
+     var scene1 = new ScrollMagic.Scene({
+       triggerElement: "#about-me", // point of execution
+       duration: ($(window).height() * 0.5) , // pin element for the window height - 1
+       triggerHook: 0, // don't trigger until #about-me hits the top of the viewport
+       reverse: true // allows the effect to trigger when scrolled in the reverse direction
+     })
+     .setPin("#color-aboutme") // the element we want to pin
+     .addTo(controller);
+
+     var scene2 = new ScrollMagic.Scene({
+       triggerElement: "#portfolio", // point of execution
+       duration: ($(window).height() *3), // pin element for the window height - 1
+       triggerHook: 0, // don't trigger until #about-me hits the top of the viewport
+       reverse: true // allows the effect to trigger when scrolled in the reverse direction
+     })
+     .setPin("#color-portfolio") // the element we want to pin
+     .addTo(controller);
